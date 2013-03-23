d60a10aadffb66e4f50965a6b1cf73d16f0dea12
eiji.ienaga <eiji.ienaga@gmail.com>
2013-03-23 14:45:30 +0900

    Initial Commit

---
+++ App/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  App
+//
+//  Created by 家永英治 on 2013/03/23.
+//  Copyright (c) 2013年 家永英治. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
