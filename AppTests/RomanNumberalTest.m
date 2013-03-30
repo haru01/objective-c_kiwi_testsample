9d202b3cb06350031736952d6fd9020cbe1ebe5b
eiji.ienaga <eiji.ienaga@gmail.com>
2013-03-31 08:37:43 +0900

    refactor

--- AppTests/RomanNumberalTest.m
+++ AppTests/RomanNumberalTest.m
@@ -8,11 +8,17 @@
 @property (readonly) NSNumber *number;
 @property (readonly) NSString *romanStr;
 
+- (id)initWithNum:(NSNumber *)aNumber;
+- (id)initWithRomanStr:(NSString *)aRomanStr;
+- (RomanNumber *)plus:(RomanNumber *)other;
+- (RomanNumber *)minus:(RomanNumber *)other;
+- (BOOL)isEqual:(id)other;
+
 @end
 
 @implementation RomanNumber
 
--(id)initWithNum:(NSNumber *)aNumber {
+- (id)initWithNum:(NSNumber *)aNumber {
     self = [super init];
     if (self) {
         _number = aNumber;
@@ -21,7 +27,7 @@
     return self;
 }
 
--(id)initWithRomanStr:(NSString *)aRomanStr {
+- (id)initWithRomanStr:(NSString *)aRomanStr {
     self = [super init];
     if (self) {
         _romanStr = aRomanStr;
@@ -30,66 +36,65 @@
     return self;
 }
 
--(NSString *) toString {
-    return [self convertRomanStr:_number];
-}
-
-
--(NSNumber *) convertNumber:(NSString *)aRomanStr {
+- (NSNumber *)convertNumber:(NSString *)aRomanStr {
     // 総当たりでマッチするものを探す方法
     for(int i=0; i<4000; i++ ) {
-        NSNumber *n = @(i);
-        NSString *other = [self convertRomanStr:@(i)];
+        NSNumber *num = @(i);
+        NSString *other = [self convertRomanStr:num];
         if ([other isEqualToString: aRomanStr]) {
-            return n;
+            return num;
         }
     }
     return @(-1);
 }
 
--(NSString *) convertRomanStr:(NSNumber *)aNumber {
-    NSDictionary *nums = @{
-       @1: @"I",
-       @4: @"IV",
-       @5: @"V",
-       @9: @"IX",
-       @10: @"X",
-       @40: @"XL",
-       @50: @"L",
-       @90: @"XC",
-       @100: @"C",
-       @400: @"CD",
-       @500: @"D",
-       @900: @"CM",
-       @1000: @"M",
-    };
-    id romanStr = [nums objectForKey:aNumber];
+- (NSString *)convertRomanStr:(NSNumber *)aNumber {
+    NSDictionary *baseNums = [RomanNumber baseNums];
+    id romanStr = [baseNums objectForKey:aNumber];
     if (romanStr) {
         return romanStr;
     }
     
-    for (NSNumber *base in [self keysDesc:nums]) {
+    for (NSNumber *base in [self keysDesc:baseNums]) {
         int diff = [aNumber intValue] - [base intValue];
         if (diff > 0) {
-            return [NSString stringWithFormat:@"%@%@",[self convertRomanStr:base], [self convertRomanStr:[NSNumber numberWithInt:diff]]];
+            return [NSString stringWithFormat:@"%@%@", [self convertRomanStr:base], [self convertRomanStr:@(diff)]];
         }
     }
     return @"";
 }
 
--(NSArray *) keysDesc:(NSDictionary *) dictionary {
-    NSMutableArray *keysDesc =[NSMutableArray arrayWithArray:Underscore.keys(dictionary)];
++ (NSDictionary *)baseNums {
+  return @{
+           @1: @"I",
+           @4: @"IV",
+           @5: @"V",
+           @9: @"IX",
+           @10: @"X",
+           @40: @"XL",
+           @50: @"L",
+           @90: @"XC",
+           @100: @"C",
+           @400: @"CD",
+           @500: @"D",
+           @900: @"CM",
+           @1000: @"M",
+           };
+}
+
+- (NSArray *)keysDesc:(NSDictionary *)dictionary {
+    NSMutableArray *keysDesc =[NSMutableArray arrayWithArray:_.keys(dictionary)];
     [keysDesc sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
         return [obj2 compare:obj1];
     }];
     return keysDesc;
 }
 
--(RomanNumber *) plus:(RomanNumber *) other {
+- (RomanNumber *)plus:(RomanNumber *)other {
     return [[RomanNumber alloc] initWithNum:@([self.number intValue] + [other.number intValue])];
 }
 
--(RomanNumber *) minus:(RomanNumber *) other {
+- (RomanNumber *)minus:(RomanNumber *)other {
     return [[RomanNumber alloc] initWithNum:@([self.number intValue] - [other.number intValue])];
 }
 
@@ -112,7 +117,7 @@
 
 @implementation TestHelper
 
-+(NSDictionary *) nums {
++ (NSDictionary *)nums {
     return @{
       @1: @"I",
       @4: @"IV",
@@ -148,7 +153,7 @@ describe(@"RomanNumber", ^{
     _.dictEach([TestHelper nums], ^(NSNumber *number, NSString *expected) {
         NSString *itStr = [NSString stringWithFormat:@"数字:%@をローマ数字:%@に変換できること", number, expected];
         it(itStr, ^{
-            RomanNumber *roman =  [[RomanNumber alloc] initWithNum:number];
+            id roman =  [[RomanNumber alloc] initWithNum:number];
             [[[roman romanStr] should] equal: expected];
         });
     });
@@ -157,23 +162,23 @@ describe(@"RomanNumber", ^{
     _.dictEach([TestHelper nums], ^(NSNumber *expected, NSString *romanStr) {
         NSString *itStr = [NSString stringWithFormat:@"ローマ数字:%@を数字:%@に変換できること", romanStr, expected];
         it(itStr, ^{
-            RomanNumber *roman =  [[RomanNumber alloc] initWithRomanStr:romanStr];
+            id roman =  [[RomanNumber alloc] initWithRomanStr:romanStr];
             [[[roman number] should] equal: expected];
         });
     });
 
     it(@"ローマ数字の足し算ができること", ^{
-        RomanNumber *one =  [[RomanNumber alloc] initWithRomanStr:@"I"];
-        RomanNumber *three =  [[RomanNumber alloc] initWithRomanStr:@"III"];
-        RomanNumber *four =  [[RomanNumber alloc] initWithRomanStr:@"IV"];
+        id one =  [[RomanNumber alloc] initWithRomanStr:@"I"];
+        id three =  [[RomanNumber alloc] initWithRomanStr:@"III"];
+        id four =  [[RomanNumber alloc] initWithRomanStr:@"IV"];
         [[[one plus:three] should] equal:four];
         [[[[one plus:three] number] should] equal:@(4)];
     });
 
     it(@"ローマ数字の引き算ができること", ^{
-        RomanNumber *ten =  [[RomanNumber alloc] initWithRomanStr:@"X"];
-        RomanNumber *three =  [[RomanNumber alloc] initWithRomanStr:@"III"];
-        RomanNumber *seven =  [[RomanNumber alloc] initWithRomanStr:@"VII"];
+        id ten =  [[RomanNumber alloc] initWithRomanStr:@"X"];
+        id three =  [[RomanNumber alloc] initWithRomanStr:@"III"];
+        id seven =  [[RomanNumber alloc] initWithRomanStr:@"VII"];
         [[[ten minus:three] should] equal:seven];
         [[[[ten minus:three] number] should] equal:@(7)];
     });
@@ -182,8 +187,6 @@ describe(@"RomanNumber", ^{
     // - ローマ数字のソート
     // - 引数が 1-3999 以外の場合の初期化
     // − 引数がローマ数字でない場合の初期化
- 
-
 });
 
 SPEC_END
