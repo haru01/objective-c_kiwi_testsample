9d5b1017417e364d4239fdc417a3e1806fcba54a
eiji.ienaga <eiji.ienaga@gmail.com>
2013-05-28 06:37:48 +0900

    change ignore

---
+++ Pods/Pods-AppTests-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Kiwi
+#define COCOAPODS_POD_AVAILABLE_Kiwi
+#define COCOAPODS_VERSION_MAJOR_Kiwi 2
+#define COCOAPODS_VERSION_MINOR_Kiwi 0
+#define COCOAPODS_VERSION_PATCH_Kiwi 5
+
+// OCHamcrest
+#define COCOAPODS_POD_AVAILABLE_OCHamcrest
+#define COCOAPODS_VERSION_MAJOR_OCHamcrest 1
+#define COCOAPODS_VERSION_MINOR_OCHamcrest 9
+#define COCOAPODS_VERSION_PATCH_OCHamcrest 0
+
+// Underscore.m
+#define COCOAPODS_POD_AVAILABLE_Underscore_m
+#define COCOAPODS_VERSION_MAJOR_Underscore_m 0
+#define COCOAPODS_VERSION_MINOR_Underscore_m 2
+#define COCOAPODS_VERSION_PATCH_Underscore_m 0
+
